bdcfc618adb7c20135b11ac227746ed44bab8ee0
Lwazi Jiyane <67145874+Lwazman@users.noreply.github.com>
2023-04-26 15:15:07 +0200

    Update size.c

--- size.c
+++ size.c
@@ -7,9 +7,11 @@
  *
  * Return: Precision.
  */
+
 int get_size(const char *format, int *i)
 {
 	int curr_i = *i + 1;
+
 	int size = 0;
 
 	if (format[curr_i] == 'l')
@@ -19,6 +21,7 @@ int get_size(const char *format, int *i)
 
 	if (size == 0)
 		*i = curr_i - 1;
+	
 	else
 		*i = curr_i;
 
